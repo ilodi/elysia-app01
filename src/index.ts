6c0a7e49bb2eec88441149c038144246d892bc10
lodi <lodi.dev@outlook.com>
2024-10-28 09:28:42 -0600

    prumer commit elysia.js and bun 2024

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+app
+  .group("/users", (app) => 
+  app
+    .get("/", ()=> "User #")
+    .get("/:id", ()=> "User AA")
+
+  )
+  .get("/", () => "Hello Elysia")
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
