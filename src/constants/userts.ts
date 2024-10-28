472e5def335efdee18aeeabdf1d9d0ef58106406
lodi <lodi.dev@outlook.com>
2024-10-28 13:14:22 -0600

    group roots

---
+++ src/constants/userts.ts
@@ -0,0 +1,8 @@
+export const users = [
+  { name: "0", id: 0, umur: 15},
+  { name: "1", id: 1, umur: 25},
+  { name: "3", id: 2, umur: 30},
+  { name: "4", id: 3, umur: 35},
+  { name: "5", id: 4, umur: 40},
+  { name: "6", id: 5, umur: 45},
+]
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 import { Elysia } from "elysia";
+import { users } from "./constants/userts";
 
 const app = new Elysia()
 
 app
   .group("/users", (app) => 
   app
-    .get("/", ()=> "User #")
-    .get("/:id", ()=> "User AA")
-
+    .get("/", ()=> users)
+    .get("/:id", ({params})=> {
+      const result = users.find((user) => user.id === Number(params.id));
+      return result ?? "User not found";
+    })
   )
-  .get("/", () => "Hello Elysia")
+  .group("/v1", (app) => 
+  app
+    .get("/", ()=> "Hello")
+    .get("/about", ()=> "About")
+    .get("/contact", ()=> "contact")
+  )
+  .group("/v2", (app) => 
+    app
+      .get("/", ()=> "🦊")
+      .get("/me", ()=> "Hi 🦊")
+    )
+  .listen(3000);
 
-app.listen(3000);
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
